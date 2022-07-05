6cb9c7487cd535ea2bb9863b5c8492ca5804520d
Nirmit Gupta <65971697+Nirmit1910@users.noreply.github.com>
2022-07-06 01:20:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const cors= require('cors');
 const xss= require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
-
+//swagger
+const swaggerUi = require('swagger-ui-express');
+const YAML= require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
@@ -43,10 +46,16 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+
+
 // routes
 app.get('/',(req,res)=>{
-  res.send('Jobs API');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 })
+
+
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs',authenticateUser,jobsRouter);
 
